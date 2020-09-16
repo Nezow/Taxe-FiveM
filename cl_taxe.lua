969415043853ca7058e1d5baa40e54f32ea49765
Nezow <56295102+Nezow@users.noreply.github.com>
2020-09-16 15:02:48 +0200

    Update cl_taxe.lua

--- cl_taxe.lua
+++ cl_taxe.lua
@@ -1,7 +1,6 @@
 Citizen.CreateThread(function()
-	Wait(6000)
 	while true do
-		Wait(3000 * 60000)
+		Wait(50*60000)
 		ShowNotification("Vous venez de payer une ~b~Taxe ~w~de ~r~10$")
 		TriggerServerEvent('taxe')
 	end
